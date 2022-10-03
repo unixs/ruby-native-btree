fa2d8249ad47896bb5c6a1e2b2b7257a0b68d765
Alexander Feodorov <vester@unixcomp.org>
2022-10-03 21:10:44 +0400

    Fix

--- ext/native_btree/iterators.c
+++ ext/native_btree/iterators.c
@@ -87,6 +87,21 @@ rbtree_each_value(VALUE self)
   return self;
 }
 
+#ifndef HAS_GTREE_NODE
+
+static gint
+reverse_foraech_callback(gpointer a, gpointer b, gpointer data)
+{
+  GPtrArray *buff = (GPtrArray *) data;
+
+  g_ptr_array_add(buff, a);
+  g_ptr_array_add(buff, b);
+
+  return FALSE;
+}
+
+#endif
+
 
 VALUE
 rbtree_reverse_each(VALUE self)
@@ -95,6 +110,7 @@ rbtree_reverse_each(VALUE self)
 
   RETURN_SIZED_ENUMERATOR(self, 0, 0, rbtree_enum_length);
 
+#ifdef HAS_GTREE_NODE
   GTreeNode *current = g_tree_node_last(rbtree->gtree);
 
   VALUE key, val;
@@ -106,6 +122,21 @@ rbtree_reverse_each(VALUE self)
 
     current = g_tree_node_previous(current);
   }
+#else
+  gint tree_size = g_tree_nnodes(rbtree->gtree);
+  GPtrArray *buff = g_ptr_array_sized_new(tree_size * 2);
+  g_tree_foreach(rbtree->gtree, reverse_foraech_callback, (gpointer) buff);
+
+  VALUE key, val;
+  for (gint i = buff->len - 1; i > 0; i--) {
+    val = (VALUE) g_ptr_array_index(buff, i);
+    key = (VALUE) g_ptr_array_index(buff, --i);
+
+    rb_yield_values(2, val, key);
+  }
+
+  g_ptr_array_free(buff, TRUE);
+#endif
 
   return self;
 }
