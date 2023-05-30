297963b3863a3b712fbcd46f86887b730872c89b
Alexander Feodorov <vester@unixcomp.org>
2023-05-30 23:56:33 +0400

    __20

--- spec/test.sh
+++ spec/test.sh
@@ -2,4 +2,16 @@
 
 source $DEV_HOME/.rvm/scripts/rvm
 
-make test
+env
+gem env
+
+rvm list
+
+pwd
+
+gem install bundler
+bundle config set --local path "vendor/ruby"
+bundle config set --local without development
+bundle install
+bundle exec rake cmake:build
+bundle exec rake spec
